d19e16a8920b3c3f0a8aa687f0ed0ce23447b830
sz16900 <sz16900@my.bristol.ac.uk>
2020-09-07 13:14:52 -1000

    error handling

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -6,21 +6,46 @@ import Loader from '../Components/Loader';
 function Product() {
   const { id } = useParams();
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const [product, setProduct] = useState({ loading: false, data: null });
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
   let content = null;
 
   useEffect(() => {
     setProduct({
       loading: true,
       data: null,
+      error: false,
     });
-    axios.get(url).then((response) => {
-      setProduct({
-        loading: false,
-        data: response.data,
+    axios
+      .get(url)
+      .then((response) => {
+        setProduct({
+          loading: false,
+          data: response.data,
+          error: false,
+        });
+      })
+      .catch(() => {
+        setProduct({
+          loading: false,
+          data: null,
+          error: true,
+        });
       });
-    });
   }, [url]);
+
+  if (product.error) {
+    content = (
+      <div>
+        <div className="bg-red-300 p-3">
+          There was an error please refresh or try again later.
+        </div>
+      </div>
+    );
+  }
 
   if (product.loading) {
     content = <Loader></Loader>;
